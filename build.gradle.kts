252d60d6a692b6821f33aa8733cff74a519269b3
Wct <nasller@qq.com>
2023-06-27 14:11:43 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
     // Java support
     id("java")
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.14.0"
+    id("org.jetbrains.intellij") version "1.14.2"
 }
 
 val artifactTypeAttribute = Attribute.of("artifactType", String::class.java)
